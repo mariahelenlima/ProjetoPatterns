8c177e3ce3fcd9ad9aa2d3e2d9a591d2c24e48f1
Maria Lima <99903075+mariahelenlima@users.noreply.github.com>
2023-09-20 20:49:06 -0300

    teste

---
+++ Fila.java
@@ -0,0 +1,23 @@
+public class fila {
+	
+	public String Nomedocumento;
+	public String Tipodocumento;
+	public String Formatodocumento;
+	public int QuantidadeDocumentos;
+	
+	
+
+	private void ImprimeDocumento() {
+		
+	}
+	
+	private void RemoveDocumento() {
+		
+	}
+	
+	private void RemoveTodosDocumentos() {
+		
+	}
+
+
+}
