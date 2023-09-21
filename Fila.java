aba6db32066ac7070d118c8059237b1e31db545a
Maria Lima <99903075+mariahelenlima@users.noreply.github.com>
2023-09-20 21:01:49 -0300

    teste2

--- Fila.java
+++ Fila.java
@@ -1,10 +1,23 @@
-public class fila {
+public class Fila {
 	
 	public String Nomedocumento;
 	public String Tipodocumento;
 	public String Formatodocumento;
 	public int QuantidadeDocumentos;
 	
+	  private static Fila instancia = null;
+
+    private Fila() {
+
+    }
+
+    public static Fila getInstance(){
+
+        if (instancia == null){
+
+            instancia = new Fila();
+
+        }
 	
 
 	private void ImprimeDocumento() {
